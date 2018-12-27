4383d52b5e0cb8a257cc56276b0e16a939452710
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-27 23:08:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,4 +13,9 @@ def line(katz_deli)
     end
   puts "The line is currently: #{line.join(" ")}"
 end
-end 
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
