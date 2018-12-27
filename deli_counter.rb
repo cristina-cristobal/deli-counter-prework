68a6ffab71796848ea5845f607ea36d2ad19fd56
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-27 23:04:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,15 @@
 katz_deli = []
 
 def line(katz_deli)
+  line = []
   if katz_deli.empty?
     puts "The line is currently empty."
-  end
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line << "#{number}. #{customer}"
+      number += 1
+    end
+  puts "The line is currently: #{line.join(" ")}"
 end
+end 
